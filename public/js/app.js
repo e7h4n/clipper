f4372515717f60864cdcbfe678963d522bfbd00d
Guo Yu <i@guoyu.me>
2013-07-06 16:01:39 +0800

    。。。

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,74 @@
+// store
+var _store = {
+  // 返回一个唯一的article id;
+  // 这个id也是articles的数组index
+  aid: function() {
+    var articles = this.fetch();
+    if (articles) {
+      return articles[articles.length].aid + 1;
+    } else {
+      return 0;
+    }
+  },
+  // 从原始数据从生成新的文章
+  set: function(value) {
+    if (key) {
+      var aid = this.aid();
+      localStorage.setItem('article' + aid,JSON.stringify(value));
+      this.push(aid);
+      return key;
+    } else {
+      return false;
+    }
+  },
+  // 保存
+  save: function(key,value) {
+    if (key) {
+      localStorage.setItem(key,value);
+      return key;
+    } else {
+      return false;
+    }
+  },
+  // 将单篇文章推送到全部文章的数组里
+  push: function(id) {
+    var articles = this.fetch();
+    var article = this.get(id);
+    articles.push(article);
+    this.save('articles',articles)
+  },
+  // 根据ID拿到单篇文章
+  get: function(key) {
+    if (key) {
+      return localStorage.getItem('article' + key) == null ? null : JSON.parse(localStorage.getItem('article' + key));
+    }
+  },
+  // 拿到所有文章
+  fetch: function() {
+    return JSON.parse(localStorage.getItem('articles'));
+  },
+  // 删除一篇文章
+  remove: function(name) {
+    var articles = this.fetch();
+    if (articles) {
+      // 首先删除单独保存的，然后删除所有文章列表中的
+      
+    } else {
+      return false;
+    }
+  }
+}
+
 //app.js
 var clipperApp = {
   ctrls: {
     articles: function($scope) {
-
+      $scope.articles = _store.fetch() == null ? [] : _store.fetch();
     },
-    article: function($scope) {
-
+    article: function($scope,$routeParams) {
+      // 最好把store抽象出来...
+      var articleID = $routeParams.rid;
+      $scope.article = _store.get(articleID);
     },
     clipper: function($scope) {
 
