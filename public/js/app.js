9b43776e303a16f0d376b2566a6ed6fd2a01311f
Guo Yu <i@guoyu.me>
2013-07-06 15:15:03 +0800

    add router

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 //app.js
+
+var clipper = angular.module('clipper', []).config(function($routeProvider) {
+  $routeProvider.
+  when('/articles', {
+    controller: lemon.ctrls.new,
+    templateUrl: '../../views/articles.html'
+  }).
+  when('/clipper', {
+    controller: lemon.ctrls.list,
+    templateUrl: '../../views/clipper.html'
+  }).
+  when('/article', {
+    controller: lemon.ctrls.list,
+    templateUrl: '../../views/list.html'
+  }).
+  otherwise({
+    redirectTo: '/clipper'
+  });
+});
