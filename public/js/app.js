2ef41c736518f0fbe821eb50153202b53a66c167
Guo Yu <i@guoyu.me>
2013-07-06 15:17:07 +0800

    eee

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,31 @@
 //app.js
+var clipperApp = {
+  ctrls: {
+    articles: function($scope) {
+
+    },
+    article: function($scope) {
+
+    },
+    clipper: function($scope) {
+
+    }
+  }
+};
 
 var clipper = angular.module('clipper', []).config(function($routeProvider) {
   $routeProvider.
   when('/articles', {
-    controller: lemon.ctrls.new,
+    controller: lemon.ctrls.articles,
     templateUrl: '../../views/articles.html'
   }).
   when('/clipper', {
-    controller: lemon.ctrls.list,
+    controller: lemon.ctrls.clipper,
     templateUrl: '../../views/clipper.html'
   }).
   when('/article', {
-    controller: lemon.ctrls.list,
-    templateUrl: '../../views/list.html'
+    controller: lemon.ctrls.article,
+    templateUrl: '../../views/article.html'
   }).
   otherwise({
     redirectTo: '/clipper'
